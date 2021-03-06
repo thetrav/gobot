61c3c0bddd9eb1ca66ce51dae5bba6546c32f737
Travis Dixon <travis.dixon@papercut.com>
2021-03-06 13:11:42 +1100

    build on OSX

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Shell
 docker run --rm -it -v "$PWD/src":/usr/src/myapp -v "$PWD/bin":/usr/src/bin -w /usr/src/myapp -e GOOS=windows -e GOARCH=amd64 x1unix/go-mingw:1.14 /bin/sh
 ```
 
+Build for OSX
+Assuming you already have go installed:
+```
+go build -v -o ../bin
+```
+
+
+
 Dependencies
 
 The go library is poorly released and you need to explicitly tag it with master (should be fine now that it's in go.mod)
@@ -25,3 +33,8 @@ libvorbisfile.dll
 which are available from https://github.com/g3n/engine/tree/master/audio/windows/bin
 
 They just need to be on the path, but I like to put them in the same folder as the executable
+
+for OSX you will need:
+```
+brew install libvorbis openal-soft
+```
--- src/main.go
+++ src/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/g3n/engine/app"
 	"github.com/g3n/engine/camera"
 	"github.com/g3n/engine/core"
-	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/gls"
-	"github.com/g3n/engine/graphic"
 	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/light"
-	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/loader/obj"
 	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/renderer"
 	"github.com/g3n/engine/util/helper"
 	"github.com/g3n/engine/window"
-	"github.com/g3n/engine/loader/obj"
-	"time"
-	"fmt"
-	"os"
-	"path/filepath"
 )
 
 type App struct {
 	*app.Application
-	scene *core.Node
+	scene  *core.Node
 	camera *camera.Camera
 }
 
@@ -121,7 +119,7 @@ func openModel(fpath string) (*core.Node, error) {
 		}
 
 		return dec.NewGroup()
-		
+
 	}
 	return nil, fmt.Errorf("Unrecognized model file extension:[%s]", ext)
 }
